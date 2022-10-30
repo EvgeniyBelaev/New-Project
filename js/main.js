764cee68ea4e948e6b7aaaad20e0060448934f20
Evgeniy <ekjek@mail.ru>
2022-10-31 01:48:50 +0300

    asd

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
 const burger = document.querySelector('#burger')
 const container = document.querySelector('.container')
-const main = document.querySelector('#main')
+
 
 
 burger.addEventListener('click', () => {
     container.classList.toggle('active')
     burger.classList.toggle('burger')
     burger.classList.toggle('btn-close')
-    main.classList.toggle('main-active')
 })
