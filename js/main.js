957725c2ab16db079edb4772d72ce9229f66c550
Evgeniy <ekjek@mail.ru>
2022-11-02 00:14:23 +0300

    created main

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const burger = document.querySelector('#burger')
 const container = document.querySelector('.container')
+const screens = document.querySelectorAll('.screen')
 
 
 
@@ -8,3 +9,34 @@ burger.addEventListener('click', () => {
     burger.classList.toggle('burger')
     burger.classList.toggle('btn-close')
 })
+
+function replaceBg(id) {
+    const bg = document.getElementById(id)
+    screens.forEach(screen => {
+        screen.style.display = 'none'
+    })
+    bg.style.display = 'block'
+}
+
+function changeBg() {
+    const links = document.querySelectorAll('.link')
+    links.forEach(link => {
+        link.addEventListener('click', e => {
+            e.preventDefault()
+            replaceBg(e.target.dataset.link)          
+        })
+        
+    })
+    const headers = document.querySelectorAll('.header')
+    headers.forEach (header => {
+        header.addEventListener('click', e => {
+            e.preventDefault()
+            container.classList.toggle('active') 
+        })
+    })
+    screens.forEach(screen => {
+        screen.style.display = 'none'
+        screens[0].style.display = 'block'
+    })
+}
+changeBg()
